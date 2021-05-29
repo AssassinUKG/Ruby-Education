0cd05ee5e1d8c17f423103b6cb1f2f21ab2b8e05
AssassinUKG <more4money@gmail.com>
2021-05-29 10:32:25 +0100

    Create simple_get.rb

---
+++ simple_get.rb
@@ -0,0 +1,35 @@
+  GNU nano 5.4                                           test.rb                                                    
+#!/usr/bin/ruby -w
+require 'net/http'
+require 'json'
+
+
+#* means take the rest of the args
+if ARGV.length < 2
+    puts "Too few arguments"
+    exit
+end
+
+if ARGV[0] == '-u'
+    $url1 = ARGV[1]
+    puts "#{$url1}"
+end
+
+def getPage
+    system("clear")
+    uri = URI($url1)
+    res = Net::HTTP.get_response(uri)
+    if (res.code == '200')
+        puts "Request completed: 200"
+    end
+    return JSON.parse(res.body)
+end
+
+flag=ARGV[0]
+if (flag == '-u')
+    r = getPage
+    puts "flag: #{flag}"
+    puts r
+else
+    puts "-u not found in command"
+end
